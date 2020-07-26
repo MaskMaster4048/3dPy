c823411549dc2a2ab23b296ccf4f02efa39a253e
MaskMaster4048 <logan104plumlee@gmail.com>
2020-07-25 20:41:57 -0500

    added rect, created a system of subobjects, and steamed out some wrinkles

--- window3d.py
+++ window3d.py
@@ -3,6 +3,7 @@ from PIL import Image, ImageTk
 from tkinter import Tk, Frame, Canvas, ALL, CENTER
 class shape:
     def __init__(self, points):
+        self.subItems=[]
         self.points = np.array(points)
         self.modelPoints = np.flip(np.rot90(self.points), 0)
         self.updatexy(np.array([[1,0,0],[0,0,1]]),np.array([[0,0]]))
@@ -10,6 +11,8 @@ class shape:
         self.xy = np.rot90(np.flip(sm.dot(self.modelPoints)+np.flip(np.rot90(tm),0),0),3)
         self.lastSM = sm
         self.lastTM = tm
+        for i in self.subItems:
+            i.updatexy(sm,tm)
     def draw(self, canvas):
         pass
     def translate(self, t):
@@ -17,21 +20,37 @@ class shape:
             point = point+t
         self.modelPoints = np.flip(np.rot90(self.points), 0)
         self.updatexy(self.lastSM, self.lastTM)
-    def init(self):
-        pass
+        for i in self.subItems:
+            i.translate(t)
 class point(shape):
     def __init__(self, point):
         super().__init__([point])
+        self.img = ImageTk.PhotoImage(Image.open("dot.jpg"))
     def draw(self, canvas):
         canvas.create_image(self.xy[0][0], self.xy[0][1], image=self.img,
                 anchor=CENTER)
     def getPoint(self):
         return self.points[0]
-    def init(self):
-        self.img = ImageTk.PhotoImage(Image.open("dot.jpg"))
 class line(shape):
     def draw(self, canvas):
         canvas.create_line(self.xy[0][0], self.xy[0][1], self.xy[1][0], self.xy[1][1])
+class rect(shape):
+    def __init__(self, points):
+        super().__init__(points)
+        for i in range(2):
+            for j in range(2):
+                self.subItems.append(line([
+                    [points[i][0], points[j][1], points[0][2]],
+                    [points[i][0], points[j][1], points[1][2]]]))
+                self.subItems.append(line([
+                    [points[i][0], points[0][1], points[j][2]],
+                    [points[i][0], points[1][1], points[j][2]]]))
+                self.subItems.append(line([
+                    [points[0][0], points[i][1], points[j][2]],
+                    [points[1][0], points[j][1], points[j][2]]]))
+    def draw(self, canvas):
+        for line in self.subItems:
+            line.draw(canvas)
 
 class screen(Canvas):
     def __init__(self, x, y):
@@ -46,7 +65,6 @@ class screen(Canvas):
             shape.updatexy(self.scaleMatrix, self.transformMatrix)
             shape.draw(self)
     def addshape(self, shape):
-        shape.init()
         self.items.append(shape)
 #depreciated
 class window(Frame):
